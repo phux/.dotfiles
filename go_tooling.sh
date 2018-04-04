28763c0c44d58cb42309a4bb494c29a9fa009fa3
Jan Mollowitz <jan.mollowitz@lesara.de>
2018-04-04 21:28:41 +0200

    Added gvm & go installation

--- go_tooling.sh
+++ go_tooling.sh
@@ -1,1 +1,8 @@
+mkdir -p ~/go/{src,bin/pkg}
+curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer | sh
+gvm install go1.4 -B
+gvm use go1.4
+export GOROOT_BOOTSTRAP=$GOROOT
+gvm install go1.10.1
+gvm use go1.10.1 --default
 gometalinter --install
