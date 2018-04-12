9e690a2abbd7d2bad3e4b53436b98b7d1a78c463
Jan Mollowitz <jan.mollowitz@lesara.de>
2018-04-12 23:11:36 +0200

    deleting obsolete files

--- i3/i3lock.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-i3lock -i ~/Pictures/screen_locked.png
--- i3/startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-feh --bg-max /home/jm/Pictures/success.jpg
-
-syndaemon -R -k -i 0.5 -d
-
-if ! urxvtc "$@"; then
-  urxvtd -q -o -f
-fi
