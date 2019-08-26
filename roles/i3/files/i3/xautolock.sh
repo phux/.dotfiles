ad948b24944996018cf02121720c70902b1bfc7d
phux <asdf>
2019-08-26 11:20:36 +0200

    i3: xautolock timer increased

--- roles/i3/files/i3/xautolock.sh
+++ roles/i3/files/i3/xautolock.sh
@@ -4,4 +4,4 @@
 # add -d to i3lock to set screen to standby directly
 TIME_TO_SLEEP=10
 
-exec xautolock -detectsleep -time 1 -locker "sh ~/.i3/i3lock.sh" -notify $TIME_TO_SLEEP -notifier "notify-send --urgency=critical -t 10000 'LOCKING screen in $TIME_TO_SLEEP seconds'" -corners ---- -cornersize 30
+exec xautolock -detectsleep -time 3 -locker "sh ~/.i3/i3lock.sh" -notify $TIME_TO_SLEEP -notifier "notify-send --urgency=critical -t 10000 'LOCKING screen in $TIME_TO_SLEEP seconds'" -corners ---- -cornersize 30
