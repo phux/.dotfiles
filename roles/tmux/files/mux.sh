d61eca80a16cf473e8c6d19f3db132e925b2df40
phux <asdf>
2019-12-12 11:08:34 +0100

    tmux: fzf & copy

--- roles/tmux/files/mux.sh
+++ roles/tmux/files/mux.sh
@@ -2,6 +2,6 @@
 set -eu
 set -o pipefail
 
-prj=$(find ~/.config/tmuxinator -exec basename {} \; | sort | uniq | fzf | cut -f 1 -d '.')
+prj=$(find ~/.config/tmuxinator -exec basename {} \; | sort | uniq  | cut -f 1 -d '.'| fzf)
 
 tmuxinator start "$prj" --suppress-tmux-version-warning=SUPPRESS-TMUX-VERSION-WARNING &>/dev/null disown
