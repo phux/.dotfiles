250baa073b81495ddc77331b6dbf7bc7b202352a
phux <janmollowitz@gmail.com>
2026-03-10 16:24:01 +0100

    update-versions.sh - minor fixes

--- scripts/update-versions.sh
+++ scripts/update-versions.sh
@@ -4,6 +4,8 @@
 
 set -euo pipefail
 
+echo "Starting version update..."
+
 VARS_FILE="group_vars/all/vars.yml"
 
 # Define the mapping between the variable names in vars.yml and their GitHub repositories.
@@ -20,24 +22,27 @@ REPOS=(
   "tmux_version:tmux/tmux"
   "hack_font_version:source-foundry/Hack"
   "fnm_version:Schniz/fnm"
-  "neovim_version:neovim/neovim"
+  # "neovim_version:neovim/neovim"
 )
 
 for repo_info in "${REPOS[@]}"; do
   VAR_NAME="${repo_info%%:*}"
   REPO="${repo_info#*:}"
 
-  echo "Checking latest version for $REPO..."
-
-  curl "https://api.github.com/repos/$REPO/releases/latest"
   LATEST_VERSION=$(curl -s "https://api.github.com/repos/$REPO/releases/latest" | grep '"tag_name":' | sed -E 's/.*"tag_name": "v?([^"]+)".*/\1/')
 
+  CURRENT_VERSION=$(grep "^$VAR_NAME:" "$VARS_FILE" | sed -E 's/^[^:]+: v?(.*)/\1/')
+
   if [[ -n "$LATEST_VERSION" ]]; then
-    echo "Latest version of $REPO is $LATEST_VERSION"
-    if [[ "$VAR_NAME" == "rg_version" ]]; then
-      sed -i "s/^$VAR_NAME: .*/$VAR_NAME: $LATEST_VERSION/" "$VARS_FILE"
+    if [[ "$LATEST_VERSION" != "$CURRENT_VERSION" ]]; then
+      echo "Updated $REPO from $CURRENT_VERSION to $LATEST_VERSION"
+      if [[ "$VAR_NAME" == "rg_version" ]]; then
+        sed -i "s/^$VAR_NAME: .*/$VAR_NAME: $LATEST_VERSION/" "$VARS_FILE"
+      else
+        sed -i "s/^$VAR_NAME: .*/$VAR_NAME: v$LATEST_VERSION/" "$VARS_FILE"
+      fi
     else
-      sed -i "s/^$VAR_NAME: .*/$VAR_NAME: v$LATEST_VERSION/" "$VARS_FILE"
+      echo "No new version for $REPO. Current version is $LATEST_VERSION"
     fi
   else
     echo "Could not determine latest version for $REPO"
@@ -45,3 +50,4 @@ for repo_info in "${REPOS[@]}"; do
 done
 
 echo "Version update complete."
+
